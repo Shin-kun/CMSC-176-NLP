cfc2b7f2b8c46d0d9d7705222df5286e20fb8b30
Gamaliel Petralba <petralbagamaliel@gmail.com>
2018-01-31 12:51:37 +0800

    count tokens and types

--- [1] Working with Regular Expressions/tokenize.pl
+++ [1] Working with Regular Expressions/tokenize.pl
@@ -15,22 +15,45 @@ while(my $line = <$fh>){
 
 close $fh or die "Can't close file : $!";
 
-my @tokens = filter(split /'s|[^a-zA-Z]+/, $text);
-say "tokens = [" . join(", ", @tokens) . "]";
+my @tokens = filter(split /'s[^a-zA-Z]+|[^a-zA-Z]+/, $text);
+say "tokens = [", join(", ", @tokens), "]";
+my $num_types = count_types(@tokens);
+say "# of tokens = ", scalar @tokens;
+say "# of types = ", $num_types;
 
 sub filter {
     my (@filtered) = @_;
     for my $token (@filtered) {
         if($token eq "ll") {
             $token = "will";
-        }elsif($token eq "d") {
+        } elsif($token eq "d") {
             $token = "would";
-        }elsif($token eq "re") {
+        } elsif($token eq "re") {
             $token = "are";
         }
     }
     return @filtered;
 }
 
+sub count_types {
+    my (@arr) = @_;
+    my @types;
 
+    for my $token (@arr) {
+        if(!exist($token, @types)) {
+            push @types, $token;
+        }
+    }
+    return scalar @types;
+}
+
+sub exist {
+    my ($token, @types) = @_;
+    for my $type (@types) {
+        if($token eq $type) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
