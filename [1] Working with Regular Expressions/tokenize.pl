400b6c43b2761be785290109c828118b33a0b918
Gamaliel Petralba <petralbagamaliel@gmail.com>
2018-01-31 12:16:33 +0800

    tokenization

--- [1] Working with Regular Expressions/tokenize.pl
+++ [1] Working with Regular Expressions/tokenize.pl
@@ -15,5 +15,22 @@ while(my $line = <$fh>){
 
 close $fh or die "Can't close file : $!";
 
+my @tokens = filter(split /'s|[^a-zA-Z]+/, $text);
+say "tokens = [" . join(", ", @tokens) . "]";
+
+sub filter {
+    my (@filtered) = @_;
+    for my $token (@filtered) {
+        if($token eq "ll") {
+            $token = "will";
+        }elsif($token eq "d") {
+            $token = "would";
+        }elsif($token eq "re") {
+            $token = "are";
+        }
+    }
+    return @filtered;
+}
+
 
 
